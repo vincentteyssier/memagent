c051d17ba8a66a00c55996417830363b59b91c08
vincentteyssier <v.teyssier@gmail.com>
2023-09-17 11:09:43 +0800

    Initial commit

---
+++ memagent_bot.py
@@ -0,0 +1,192 @@
+import logging, os, sys
+from dotenv import load_dotenv
+
+from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+
+# initialize the logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# LlamaIndex LLM/VectorStore/embeddings
+import faiss
+from langchain import OpenAI
+from langchain.llms import AzureOpenAI
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from local_embeddings import LocalHuggingFaceEmbeddings
+
+from llama_index import (
+    SimpleDirectoryReader,
+    load_index_from_storage,
+    VectorStoreIndex,
+    StorageContext,
+)
+from llama_index.vector_stores.faiss import FaissVectorStore
+
+# initialize the vector store
+# dimensions of text-ada-embedding-002
+d = 1536
+faiss_index = faiss.IndexFlatL2(d)
+vector_store = FaissVectorStore(faiss_index=faiss_index)
+
+# load documents
+documents = SimpleDirectoryReader("../paul_graham_essay/data").load_data()
+
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+
+# save index to disk
+index.storage_context.persist()
+
+
+# load index from disk
+vector_store = FaissVectorStore.from_persist_dir("./storage")
+storage_context = StorageContext.from_defaults(
+    vector_store=vector_store, persist_dir="./storage"
+)
+index = load_index_from_storage(storage_context=storage_context)
+
+# set Logging to DEBUG for more detailed outputs
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(f"<b>{response}</b>")
+# set Logging to DEBUG for more detailed outputs
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do after his time at Y Combinator?")
+print(f"<b>{response}</b>")
+
+
+
+
+
+load_dotenv()
+
+# Store bot screaming status
+screaming = False
+
+# Pre-assign menu text
+FIRST_MENU = "<b>Menu 1</b>\n\nStore note"
+SECOND_MENU = "<b>Menu 2</b>\n\nRetrieve memory"
+
+# Pre-assign button text
+NEXT_BUTTON = "Next"
+BACK_BUTTON = "Back"
+TUTORIAL_BUTTON = "Tutorial"
+
+# Build keyboards
+FIRST_MENU_MARKUP = InlineKeyboardMarkup([[
+    InlineKeyboardButton(NEXT_BUTTON, callback_data=NEXT_BUTTON)
+]])
+SECOND_MENU_MARKUP = InlineKeyboardMarkup([
+    [InlineKeyboardButton(BACK_BUTTON, callback_data=BACK_BUTTON)],
+    [InlineKeyboardButton(TUTORIAL_BUTTON, url="https://core.telegram.org/bots/api")]
+])
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """
+    This function would be added to the dispatcher as a handler for messages coming from the Bot API
+    """
+
+    # Print to console
+    print(f'{update.message.from_user.first_name} wrote {update.message.text}')
+
+    if screaming and update.message.text:
+        context.bot.send_message(
+            update.message.chat_id,
+            update.message.text.upper(),
+            # To preserve the markdown, we attach entities (bold, italic...)
+            entities=update.message.entities
+        )
+    else:
+        # This is equivalent to forwarding, without the sender's name
+        update.message.copy(update.message.chat_id)
+
+
+def scream(update: Update, context: CallbackContext) -> None:
+    """
+    This function handles the /scream command
+    """
+
+    global screaming
+    screaming = True
+
+
+def whisper(update: Update, context: CallbackContext) -> None:
+    """
+    This function handles /whisper command
+    """
+
+    global screaming
+    screaming = False
+
+
+def menu(update: Update, context: CallbackContext) -> None:
+    """
+    This handler sends a menu with the inline buttons we pre-assigned above
+    """
+
+    context.bot.send_message(
+        update.message.from_user.id,
+        FIRST_MENU,
+        parse_mode=ParseMode.HTML,
+        reply_markup=FIRST_MENU_MARKUP
+    )
+
+
+def button_tap(update: Update, context: CallbackContext) -> None:
+    """
+    This handler processes the inline buttons on the menu
+    """
+
+    data = update.callback_query.data
+    text = ''
+    markup = None
+
+    if data == NEXT_BUTTON:
+        text = SECOND_MENU
+        markup = SECOND_MENU_MARKUP
+    elif data == BACK_BUTTON:
+        text = FIRST_MENU
+        markup = FIRST_MENU_MARKUP
+
+    # Close the query to end the client-side loading animation
+    update.callback_query.answer()
+
+    # Update message content with corresponding menu section
+    update.callback_query.message.edit_text(
+        text,
+        ParseMode.HTML,
+        reply_markup=markup
+    )
+
+
+def main() -> None:
+    updater = Updater(os.getenv('BOT_TOKEN'))
+
+    # Get the dispatcher to register handlers
+    # Then, we register each handler and the conditions the update must meet to trigger it
+    dispatcher = updater.dispatcher
+
+    # Register commands
+    dispatcher.add_handler(CommandHandler("scream", scream))
+    dispatcher.add_handler(CommandHandler("whisper", whisper))
+    dispatcher.add_handler(CommandHandler("menu", menu))
+
+    # Register handler for inline buttons
+    dispatcher.add_handler(CallbackQueryHandler(button_tap))
+
+    # Echo any message that is not a command
+    dispatcher.add_handler(MessageHandler(~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
